aaaf51380b812ebb767e559138fea0942f2f4c14
susi1603 <susyreyes09@gmail.com>
2021-06-29 13:18:38 -0500

    update

--- Source/Arkanoid/Paddle_PlayerController.cpp
+++ Source/Arkanoid/Paddle_PlayerController.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Paddle_PlayerController.h"
 #include "Kismet/GameplayStatics.h"
 #include "Camera/CameraActor.h"
@@ -9,25 +8,29 @@
 
 APaddle_PlayerController::APaddle_PlayerController()
 {
-
 }
 
 void APaddle_PlayerController::BeginPlay()
 {
-    TArray<AActor*> CameraActors; 
-    UGameplayStatics::GetAllActorsOfClass(GetWorld(),ACameraActor::StaticClass(), CameraActors);
+    TArray<AActor *> CameraActors;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACameraActor::StaticClass(), CameraActors);
 
-    FViewTargetTransitionParams Params; 
+    FViewTargetTransitionParams Params;
     SetViewTarget(CameraActors[0], Params);
 }
 
 void APaddle_PlayerController::SetupInputComponent()
 {
-
+    Super::SetupInputComponent();
+    EnableInput(this);
+    InputComponent->BindAxis("MoveHorizontal", this, &APaddle_PlayerController::MoveHorizontal);
 }
-
 
 void APaddle_PlayerController::MoveHorizontal(float AxisValue)
 {
-    
+    auto MyPawn = Cast<APaddle>(GetPawn());
+    if (MyPawn)
+    {
+        MyPawn->MoveHorizontal(AxisValue);
+    }
 }
